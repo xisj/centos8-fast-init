9933088b40cccef2c62c7ab9ac974a0ef15a85b3
terry2010 <jstel@126.com>
2020-04-19 23:01:13 +0800

    Update fast.sh

--- vps/fast.sh
+++ vps/fast.sh
@@ -1,7 +1,7 @@
 cat /proc/cpuinfo
 cat /etc/redhat-release 
 
-yum install -y wget git vim lrzsz screen net-tools telnet iftop bind-utils &&\
+dnf install -y wget git vim lrzsz screen net-tools telnet iftop bind-utils &&\
 curl -o /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-8.repo &&\
 dnf install -y epel-release &&\
 dnf makecache &&\
@@ -24,11 +24,12 @@ docker run -d -p 9999:9999 -p 9999:9999/udp --restart=always --name ss-go -v /da
 #firewall-cmd --zone=public --add-port=9999/udp --permanent  
 #firewall-cmd --reload
 
+#启用bbr
 echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
 echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
 sysctl -p
 reboot
 
-
+#查看bbr是否生效
 sysctl -n net.ipv4.tcp_congestion_control
 lsmod | grep bbr
