72c28a1a2fdec3c3a75222a267f4950292791ba3
terry2010 <jstel@126.com>
2020-04-21 18:39:12 +0800

    Update and rename install.sh to install_cn.sh

--- docker/install_cn.sh
+++ docker/install_cn.sh
@@ -1,3 +1,4 @@
+#在中国大陆网络环境下， 利用aliyun正常安装docker和相关服务
 dnf install -y yum-utils  device-mapper-persistent-data  lvm2
 dnf config-manager --add-repo=https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
 dnf install -y https://mirrors.aliyun.com/docker-ce/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.13-3.1.el7.x86_64.rpm
