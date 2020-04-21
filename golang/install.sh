b441a03dfa3b3104edff1c6171c4e5f859e91ad8
terry2010 <jstel@126.com>
2020-04-21 21:31:40 +0800

    Update install.sh

--- golang/install.sh
+++ golang/install.sh
@@ -1,2 +1,6 @@
 wget https://dl.google.com/go/go1.14.2.linux-amd64.tar.gz
 tar -C /usr/local -xzf go1.14.2.linux-amd64.tar.gz
+
+echo '
+PATH=$PATH:/usr/local/go/bin' > ~/.bashrc  
+source ~/.bashrc  
