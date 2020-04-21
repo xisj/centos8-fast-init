9a36276495da0b4b03c91a5a40c74664afd73413
terry2010 <jstel@126.com>
2020-04-21 17:47:15 +0800

    更改为aliyun的源地址

--- docker/install.sh
+++ docker/install.sh
@@ -1,6 +1,6 @@
 dnf install -y yum-utils  device-mapper-persistent-data  lvm2
-dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
-dnf install -y https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.6-3.3.el7.x86_64.rpm
+dnf config-manager --add-repo=https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+dnf install -y https://mirrors.aliyun.com/docker-ce/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.13-3.1.el7.x86_64.rpm
 dnf install -y docker-ce docker-ce-cli
 systemctl start docker
 systemctl enable docker
