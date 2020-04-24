0de9c9c0b85f3158c299d41cfe43df702a1e0fc6
terry2010 <jstel@126.com>
2020-04-24 10:45:17 +0800

    Update samba.sh

--- vps/samba.sh
+++ vps/samba.sh
@@ -5,6 +5,14 @@ systemctl start smb
 systemctl enable smb
 systemctl status smb
 
+#打开端口
+firewall-cmd --zone=public --add-port=139/tcp --permanent
+firewall-cmd --zone=public --add-port=445/tcp --permanent
+firewall-cmd --zone=public --add-port=137/udp --permanent
+firewall-cmd --zone=public --add-port=138/udp --permanent
+ 
+firewall-cmd --reload
+
 
 #########当共享root 文件夹的时候， 默认没有写权限
 #临时授予，重启后消失
@@ -13,3 +21,4 @@ setenforce 0
 #永久授予
 /etc/selinux/config
 #将SELINUX=enforcing改为SELINUX=disabled
+
